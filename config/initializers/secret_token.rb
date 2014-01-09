8e0c3301abc9f6b96cf0d6b10c86d0607a6ef620
sulin <sulin@sulin-VirtualBox.(none)>
2014-01-09 23:12:46 +0800

    new project depot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '61c518fd06dd56e032b59253c228d852d60d155eb9e2537febb5175ee9eb644e46859c87b1cc3fd4041b96bce476d24755d976cab0a4922ab4846cd681aebaa1'
